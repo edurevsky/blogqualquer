d541e7df6ca79075fa5f92abd2d19b7ca89ee18c
edurevsky <edurevsky@gmail.com>
2022-05-31 09:07:20 -0300

    first commit

---
+++ src/main/kotlin/me/edurevsky/blog/blogqualquer/BlogqualquerApplication.kt
@@ -0,0 +1,11 @@
+package me.edurevsky.blog.blogqualquer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogqualquerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogqualquerApplication>(*args)
+}
---
+++ src/test/kotlin/me/edurevsky/blog/blogqualquer/BlogqualquerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.edurevsky.blog.blogqualquer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogqualquerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
