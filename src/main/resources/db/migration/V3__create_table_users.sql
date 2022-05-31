9fd96c9bcf645267c12a4e8bfbf937379580e4ff
edurevsky <edurevsky@gmail.com>
2022-05-31 15:28:14 -0300

    add migration create table users

---
+++ src/main/resources/db/migration/V3__create_table_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    complete_name VARCHAR(150) NOT NULL
+);
