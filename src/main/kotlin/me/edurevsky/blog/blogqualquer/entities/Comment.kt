7073299c7e1cc46c29a0c03eb64d3b56b0f1c6ff
edurevsky <edurevsky@gmail.com>
2022-06-09 10:05:16 -0300

    update Comment orm and updated kotlin version at pom

--- src/main/kotlin/me/edurevsky/blog/blogqualquer/entities/Comment.kt
+++ src/main/kotlin/me/edurevsky/blog/blogqualquer/entities/Comment.kt
@@ -1,5 +1,6 @@
 package me.edurevsky.blog.blogqualquer.entities
 
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
@@ -11,5 +12,15 @@ data class Comment(
     @ManyToOne
     @JoinColumn(name = "post_id")
     val post: Post? = null,
-    val content: String? = null
+    val content: String? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    val author: User? = null,
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime? = null,
+
+    @Column(name = "updated_at")
+    val updatedAt: LocalDateTime? = null
 )
