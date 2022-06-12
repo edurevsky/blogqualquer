273134945575b9306ddff47de0611579d297c631
edurevsky <edurevsky@gmail.com>
2022-06-12 20:32:53 -0300

    add migration insert into topics

---
+++ src/main/resources/db/migration/V14__insert_into_topics.sql
@@ -0,0 +1,14 @@
+INSERT INTO topics (id, `name`)
+VALUES (1, 'Java Programming');
+
+INSERT INTO topics (id, `name`)
+VALUES (2, 'Kotlin Programming');
+
+INSERT INTO topics (id, `name`)
+VALUES (3, 'Web Frameworks');
+
+INSERT INTO topics (id, `name`)
+VALUES (4, 'Functional Programming');
+
+INSERT INTO topics (id, `name`)
+VALUES (5, 'Test Driven Development');
